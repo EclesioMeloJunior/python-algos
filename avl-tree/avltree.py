c4033a9769e029536629efa8f639252a6c53f4d3
eclesiomelojunior <eclesiomelo.1@gmail.com>
2020-10-06 18:31:02 -0300

    chore: missing rotate left/right subtrees

--- avl-tree/avltree.py
+++ avl-tree/avltree.py
@@ -1,8 +1,13 @@
 class Node:
-    def __init__(self, value: int):
+    def __init__(self, value: int, bf: int = 0):
         self.value = value
         self.left = None
         self.right = None
+        self.parent = None
+
+        self.bf = bf
+        self.height = 1
+
 
 
 class AVLTree:
@@ -32,14 +37,51 @@ class AVLTree:
         print(r.value)
         self.print_recursive(r.right)
 
+    def _get_height(self, root: Node) -> int:
+        if root is None:
+            return 0
+
+        return root.height
+
     def _add_to_root(self, root: Node, n: Node) -> Node:
         if root is None:
             return n
 
         if root.value > n.value:
-            root.left = self._add_to_root(root.left, n)
+            subtree_left = self._add_to_root(root.left, n)
+
+            root.left = subtree_left
+            subtree_left.parent = root.left
 
         if root.value < n.value:
-            root.right = self._add_to_root(root.right, n)
+            subtree_right = self._add_to_root(root.right, n)
 
-        return root
+            root.right = subtree_right
+            subtree_right.parent = root.right
+        
+        l_height = self._get_height(root.left)
+        r_height = self._get_height(root.right)
+
+        root.height = max(l_height, r_height) + 1
+        root.bf = l_height - r_height
+
+        return self._balance(root)
+
+
+    def _balance(self, root: Node) -> Node:
+        if root.bf == 2:
+            if root.left.bf < 0:
+                root.left = self._rotate_left(root.left)
+                return self._rotate_right(root)
+            else:
+                return self._rotate_right(root)
+
+        elif root.bf == -2:
+            if root.right.bf > 0:
+                root.right = self._rotate_right(root.right)
+                return self._rotate_left(root)
+            else:
+                return self._rotate_left(root)
+
+        else:
+            return root
